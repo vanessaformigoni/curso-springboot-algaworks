39c3b68e1f6bec6405ebcd3561ff5d17b94b782b
Vanessa <vanessa.formigoni@hotmail.com>
2020-10-28 21:47:11 -0300

    Fazendo consultsa dinamica com o criaeriaApi

--- algafood-api/src/main/java/com/algaworks/algafoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 
 import com.algaworks.algafoodapi.domain.Repository.RestauranteRepositoryQueries;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
@@ -22,7 +24,7 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
     @Override
     public List<Restaurante> find(String nome,
                                   BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-//Incio Exemplo 01
+// Incio Exemplo 01
 //        var jpql = "from Restaurante where nome like :nome "
 //                + "and taxaFrete between :taxaInicial and :taxaFinal"; //Exemplo 01: query jpql montada (inativo)
         //return manager.createQuery(jpql, Restaurante.class //Para o exemplo 01
@@ -32,32 +34,40 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 //                .getResultList();
 // Fim Exemplo 01
 
-        var jpql = new StringBuilder(); //Exemplo 02: jpql com consulta dinamica (em uso)
-        jpql.append("from Restaurante where 0=0 ");
+// Inicio Exemplo 02
+//        var jpql = new StringBuilder(); //Exemplo 02: jpql com consulta dinamica (inativo)
+//        jpql.append("from Restaurante where 0=0 ");
+//
+//        var parametros = new HashMap<String, Object>();
+//
+//        if (StringUtils.hasLength(nome)) {
+//            jpql.append("and nome like :nome ");
+//            parametros.put("nome", "%" + nome + "%");
+//        }
+//
+//        if (taxaFreteInicial != null) {
+//            jpql.append("taxaFrate >= :taxaFreteInicial");
+//            parametros.put("taxaInicial", taxaFreteInicial);
+//        }
+//
+//        if (taxaFreteFinal != null) {
+//            jpql.append("and taxaFrete <= :taxaFinal ");
+//            parametros.put("taxaFinal", taxaFreteFinal);
+//        }
+//
+//        TypedQuery<Restaurante> query = manager
+//                .createQuery(jpql.toString(), Restaurante.class);
+//
+//        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+//
+//        return query.getResultList()
+// Fim Exemplo 02
 
-        var parametros = new HashMap<String, Object>();
+        CriteriaBuilder builder = manager.getCriteriaBuilder(); // Exemplo 03: Consulta usando CriteriaApi
+        CriteriaQuery<Restaurante> criteriaQuery = builder.createQuery(Restaurante.class);
+        criteriaQuery.from(Restaurante.class); //clausula from
 
-        if (StringUtils.hasLength(nome)) {
-            jpql.append("and nome like :nome ");
-            parametros.put("nome", "%" + nome + "%");
-        }
-
-        if (taxaFreteInicial != null) {
-            jpql.append("taxaFrate >= :taxaFreteInicial");
-            parametros.put("taxaInicial", taxaFreteInicial);
-        }
-
-        if (taxaFreteFinal != null) {
-            jpql.append("and taxaFrete <= :taxaFinal ");
-            parametros.put("taxaFinal", taxaFreteFinal);
-        }
-
-        TypedQuery<Restaurante> query = manager
-                .createQuery(jpql.toString(), Restaurante.class);
-
-        parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-
+        TypedQuery<Restaurante> query = manager.createQuery(criteriaQuery);
         return query.getResultList();
-
     }
 }
