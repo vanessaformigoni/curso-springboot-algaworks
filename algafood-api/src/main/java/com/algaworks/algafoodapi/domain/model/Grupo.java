4921b5d0eac9da12f306cc43a55da37ff7b71340
Vanessa L M Formigoni <vanessapessoal@PPM-VIX-1979.local>
2021-04-15 21:47:30 -0300

    Criando as entidades grupo e usuario e mapeando a relação entre eles

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Grupo.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Grupo {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"), //entender isso melhor
+            inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
+}
