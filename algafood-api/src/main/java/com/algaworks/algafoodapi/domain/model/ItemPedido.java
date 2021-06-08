e570ecde9430f9a5a092c826a69d18bbc7d98388
Vanessa Pessoal <vanessapessoal@PPM-VIX-1979.local>
2021-06-08 19:15:38 -0300

    7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/ItemPedido.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ItemPedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private String observao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+
+}
