eb72d162726342166da658efbfbe7310c4f822ff
Vanessa Pessoal <vanessapessoal@PPM-VIX-1979.local>
2021-06-14 19:35:30 -0300

    comentando as classes desligadas pra não causar conflito

--- algafood-api/src/main/java/com/algaworks/algafoodapi/zdescartados/__CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/zdescartados/__CozinhaController.java
@@ -1,66 +1,66 @@
-package com.algaworks.algafoodapi.zdescartados;
-
-import com.algaworks.algafoodapi.domain.Repository.CozinhaRepository;
-import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
-import com.algaworks.algafoodapi.domain.model.Cozinha;
-import com.algaworks.algafoodapi.domain.service.CadastroCozinhaService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-import java.util.Optional;
-
-@RestController
-@RequestMapping(value="/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
-public class __CozinhaController {
-
-    @Autowired
-    private CozinhaRepository cozinhaRepository;
-
-    @Autowired
-    private CadastroCozinhaService cadastroCozinhaService;
-
-    @GetMapping
-    public List<Cozinha> listar() {
-        return cozinhaRepository.findAll();
-    }
-
-    @GetMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-        Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
-
-        if(cozinha.isPresent())
-            return ResponseEntity.ok(cozinha.get());
-            // return ResponseEntity.status(HttpStatus.OK).body(cozinha); //opcao 02
-        else
-            return ResponseEntity.notFound().build();
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cozinha adicionar (@RequestBody Cozinha cozinha) {
-        return cadastroCozinhaService.salvar(cozinha);
-    }
-
-    @PutMapping("/{cozinhaId}")
-    public ResponseEntity<Cozinha> atualizar (@PathVariable Long cozinhaId,
-                                              @RequestBody Cozinha cozinha) {
-        Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
-
-        if(cozinhaAtual.isPresent()) {
-            BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
-
-            Cozinha cozinhaSalva = cadastroCozinhaService.salvar(cozinhaAtual.get());
-            return ResponseEntity.ok().body(cozinhaSalva);
-        }
-        return ResponseEntity.notFound().build();
-    }
+//package com.algaworks.algafoodapi.zdescartados;
+//
+//import com.algaworks.algafoodapi.domain.Repository.CozinhaRepository;
+//import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
+//import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
+//import com.algaworks.algafoodapi.domain.model.Cozinha;
+//import com.algaworks.algafoodapi.domain.service.CadastroCozinhaService;
+//import org.springframework.beans.BeanUtils;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@RestController
+//@RequestMapping(value="/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+//public class __CozinhaController {
+//
+//    @Autowired
+//    private CozinhaRepository cozinhaRepository;
+//
+//    @Autowired
+//    private CadastroCozinhaService cadastroCozinhaService;
+//
+//    @GetMapping
+//    public List<Cozinha> listar() {
+//        return cozinhaRepository.findAll();
+//    }
+//
+//    @GetMapping("/{cozinhaId}")
+//    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+//        Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+//
+//        if(cozinha.isPresent())
+//            return ResponseEntity.ok(cozinha.get());
+//            // return ResponseEntity.status(HttpStatus.OK).body(cozinha); //opcao 02
+//        else
+//            return ResponseEntity.notFound().build();
+//    }
+//
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Cozinha adicionar (@RequestBody Cozinha cozinha) {
+//        return cadastroCozinhaService.salvar(cozinha);
+//    }
+//
+//    @PutMapping("/{cozinhaId}")
+//    public ResponseEntity<Cozinha> atualizar (@PathVariable Long cozinhaId,
+//                                              @RequestBody Cozinha cozinha) {
+//        Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
+//
+//        if(cozinhaAtual.isPresent()) {
+//            BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
+//
+//            Cozinha cozinhaSalva = cadastroCozinhaService.salvar(cozinhaAtual.get());
+//            return ResponseEntity.ok().body(cozinhaSalva);
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
 
     //    @DeleteMapping("/{cozinhaId}")
 //    public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
@@ -76,10 +76,10 @@ public class __CozinhaController {
 //                    .body(e.getMessage());
 //        }
 //    }
-    @DeleteMapping("/{cozinhaId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) //Em caso de sucesso, retorna isso.
-    public void remover(@PathVariable Long cozinhaId) {
-        cadastroCozinhaService.excluir(cozinhaId);
-    }
-}
+//    @DeleteMapping("/{cozinhaId}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT) //Em caso de sucesso, retorna isso.
+//    public void remover(@PathVariable Long cozinhaId) {
+//        cadastroCozinhaService.excluir(cozinhaId);
+//    }
+//}
 
--- algafood-api/src/main/java/com/algaworks/algafoodapi/zdescartados/__EntidadeNaoEncontradaException.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/zdescartados/__EntidadeNaoEncontradaException.java
@@ -1,19 +1,19 @@
-package com.algaworks.algafoodapi.zdescartados;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-//@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada") //não vou usar mais
-public class __EntidadeNaoEncontradaException extends ResponseStatusException {
-
-    private static final long serialVersionUID = 1L;
-
-    public __EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
-        super(status, mensagem);
-    }
-
-    public __EntidadeNaoEncontradaException(String mensagem) {
-        this(HttpStatus.NOT_FOUND,mensagem);
-    }
-}
-
+//package com.algaworks.algafoodapi.zdescartados;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.server.ResponseStatusException;
+//
+////@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada") //não vou usar mais
+//public class __EntidadeNaoEncontradaException extends ResponseStatusException {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    public __EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+//        super(status, mensagem);
+//    }
+//
+//    public __EntidadeNaoEncontradaException(String mensagem) {
+//        this(HttpStatus.NOT_FOUND,mensagem);
+//    }
+//}
+//
