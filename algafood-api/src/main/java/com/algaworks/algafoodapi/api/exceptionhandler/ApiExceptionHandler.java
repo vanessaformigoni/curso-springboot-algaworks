a35374a19042aa0571ebf0c53f6027765970020c
Vanessa Pessoal <vanessapessoal@PPM-VIX-1979.local>
2021-07-14 20:01:45 -0300

    8.13 implementando exception handler

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.algaworks.algafoodapi.api.exceptionhandler;
 
+import com.algaworks.algafoodapi.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafoodapi.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -38,14 +38,26 @@ public class ApiExceptionHandler {
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
+    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException(EntidadeEmUsoException e) {
 
         Problema problema = Problema.builder()
                 .dataHora(LocalDateTime.now())
-                .mensagem("O tipo de midia nao é aceito").build();
+                .mensagem(e.getMessage()).build();
 
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
                 .body(problema);
 
     }
+
+    @ExceptionHandler(EntidadeEmUsoException.class)
+    public ResponseEntity<?> tratarEntidadeEmUsoException(EntidadeEmUsoException e) {
+
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage()).build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(problema);
+    }
+
 }
--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/exception/EntidadeNaoEncontradaException.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,9 +1,6 @@
 package com.algaworks.algafoodapi.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada")
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)//, reason = "Entidade não encontrada")
 public abstract class EntidadeNaoEncontradaException extends NegocioException {
 
     private static final long serialVersionUID = 1L;
