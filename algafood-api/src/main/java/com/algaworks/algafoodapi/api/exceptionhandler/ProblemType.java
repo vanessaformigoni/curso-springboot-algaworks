e718c8271d5ee379507df7ed6a4b4e87d7c8cc4b
Vanessa Pessoal <vanessapessoal@ip-10-210-128-240.ec2.internal>
2021-12-13 17:08:45 -0300

    9.3. Desafio: tratando exception de violação de constraints de validação

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ProblemType.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/exceptionhandler/ProblemType.java
@@ -9,8 +9,9 @@ public enum ProblemType {
     RECURSO_NAO_ENCONTRADO("/recurso-nao-econtrado", "Recurso não encontrado"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
     ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
-    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
 
     private String title;
     private String uri;
