e71cdb23fd741f5c094f17125856db3338927013
Vanessa Pessoal <vanessapessoal@ip-10-210-128-240.ec2.internal>
2021-12-13 17:04:41 -0300

    9.2. Adicionando constraints e validando no controller com @Valid

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ public class Restaurante  {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotNull
     @Column(nullable = false)
     private String nome;
 
