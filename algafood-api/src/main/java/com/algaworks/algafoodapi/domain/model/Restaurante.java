a06ad68d5c83642eb4d70b8431092223b49bacfd
Vanessa Pessoal <vanessapessoal@PPM-VIX-1979.local>
2021-12-19 15:40:45 -0300

    9.10. Customizando mensagens de validação na anotação da constraint

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -28,7 +28,7 @@ public class Restaurante  {
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotBlank//groups = Groups.CadastroRestaurante.class) //Usando grupos pra fazer a validacao
+    @NotBlank(message = "Nome é obrigatorio")//groups = Groups.CadastroRestaurante.class) //Usando grupos pra fazer a validacao
     @Column(nullable = false)
     private String nome;
 
