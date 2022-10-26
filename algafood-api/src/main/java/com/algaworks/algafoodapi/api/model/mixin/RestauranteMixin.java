a8035230bd5a078dfc01deaef7db15a0907c6f42
vanessaformigoni <vanessa.formigoni@hotmail.com>
2022-10-25 21:29:20 -0300

    11.8. Desafio: refatorando o código para usar OffsetDateTime

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/model/mixin/RestauranteMixin.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/model/mixin/RestauranteMixin.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafoodapi.api.model.mixin;
 
-import com.algaworks.algafoodapi.core.validation.Groups;
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.model.Endereco;
 import com.algaworks.algafoodapi.domain.model.FormaPagamento;
@@ -8,17 +7,8 @@ import com.algaworks.algafoodapi.domain.model.Produto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.groups.ConvertGroup;
-import javax.validation.groups.Default;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class RestauranteMixin {
@@ -31,11 +21,11 @@ public abstract class RestauranteMixin {
     @JsonIgnore
     private Endereco endereco;
 
- //   @JsonIgnore
+    @JsonIgnore
     @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss'Z'")
     private OffsetDateTime dataCadastro;
 
- //   @JsonIgnore
+    @JsonIgnore
     @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss'Z'")
     private OffsetDateTime dataAtualizacao;
 
