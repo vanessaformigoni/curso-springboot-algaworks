b9a2fa5dedb82f40acbd4aeafbb2c7c30152ad43
vanessaformigoni <vanessa.formigoni@hotmail.com>
2023-02-05 15:27:05 -0300

    11.14. Adicionando e usando o ModelMapper

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteInputDisassembler.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteInputDisassembler.java
@@ -3,20 +3,17 @@ package com.algaworks.algafoodapi.api.assembler;
 import com.algaworks.algafoodapi.api.input.RestauranteInput;
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RestauranteInputDisassembler {
+public class RestauranteInputDisassembler { //Desmonta de um entidade de modelo de entrada pra uma entidade de dominio.
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Restaurante toDomainObject (RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteModelAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteModelAssembler.java
@@ -3,6 +3,8 @@ package com.algaworks.algafoodapi.api.assembler;
 import com.algaworks.algafoodapi.api.model.CozinhaModel;
 import com.algaworks.algafoodapi.api.model.RestauranteModel;
 import com.algaworks.algafoodapi.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,17 +12,11 @@ import java.util.stream.Collectors;
 
 @Component
 public class RestauranteModelAssembler { //Entidade monta um Model
-    public RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(restaurante.getCozinha().getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
 
-        RestauranteModel restauranteModel = new RestauranteModel();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteModel.setCozinha(cozinhaModel);
-        return restauranteModel;
+    @Autowired
+    private ModelMapper modelMapper;
+    public RestauranteModel toModel(Restaurante restaurante) {
+        return modelMapper.map(restaurante,RestauranteModel.class);
     }
 
     public List<RestauranteModel> toCollectionModel (List<Restaurante> restaurantes) {
