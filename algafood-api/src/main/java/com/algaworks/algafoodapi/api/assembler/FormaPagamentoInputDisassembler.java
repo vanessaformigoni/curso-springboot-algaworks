0bc8ae3b2f882bec5baa43b97d5b6ce4df17ed82
vanessaformigoni <vanessa.formigoni@hotmail.com>
2023-08-20 20:32:13 -0300

    12.5. Desafio: implementando os endpoints de formas de pagamento

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/FormaPagamentoInputDisassembler.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafoodapi.api.assembler;
+
+import com.algaworks.algafoodapi.api.input.FormaPagamentoInput;
+import com.algaworks.algafoodapi.domain.model.FormaPagamento;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FormaPagamentoInputDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public FormaPagamento toDomainObject(FormaPagamentoInput formaPagamentoInput) {
+        return modelMapper.map(formaPagamentoInput, FormaPagamento.class);
+    }
+
+    public void copyToDomainObject(FormaPagamentoInput formaPagamentoInput, FormaPagamento formaPagamento) {
+        modelMapper.map(formaPagamentoInput, formaPagamento);
+    }
+}
---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/model/FormaPagamentoModel.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class FormaPagamentoModel {
+
+    private Long id;
+    private String descricao;
+
+}
