68be52fad05d13fb4f0d9c0ad43971d4cbb7ec3b
vanessaformigoni <vanessa.formigoni@hotmail.com>
2023-09-23 18:29:13 -0300

    12.11. Implementando regra de negócio para evitar usuários com e-mails duplicados

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/UsuarioRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/UsuarioRepository.java
@@ -2,5 +2,9 @@ package com.algaworks.algafoodapi.domain.repository;
 
 import com.algaworks.algafoodapi.domain.model.Usuario;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 }
--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/service/CadastroUsuarioService.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/service/CadastroUsuarioService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 
 @Service
 public class CadastroUsuarioService {
@@ -17,6 +19,15 @@ public class CadastroUsuarioService {
 
     @Transactional
     public Usuario salvar(Usuario usuario) {
+        usuarioRepository.detach(usuario);
+
+        Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+        if(usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+            throw new NegocioException(
+                    String.format("Já existe um usuário cadastrado com o email %s", usuario.getEmail()));
+        }
+
         return usuarioRepository.save(usuario);
     }
 
